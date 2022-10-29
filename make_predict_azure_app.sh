fc039781a35dc0df0447bb801b2c11dc014f5e79
kottman <kwstaskottas@gmail.com>
2022-10-29 05:57:54 +0000

    Update app url

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://mywebapp213412.azurewebsites.net:$PORT/predict
+     -X POST https://mywebapp213498.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
